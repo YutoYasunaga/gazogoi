8b7e05c2c5777017060422e868260c0248824cb1
YutoYasunaga <yuto.yasunaga@gmail.com>
2018-08-30 00:25:22 +0900

    Modified RSpec category controller

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
+  describe "POST #create" do
+    let(:category_attributes) { FactoryGirl.attributes_for(:category) }
+    it 'saves new category' do
+      post :create, params: { category: category_attributes }
+      expect(Category.count).to eq(1)
+    end
+  end
+
 end
