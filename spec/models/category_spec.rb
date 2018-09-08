433649aee7b8fb90f646e608fd8e1c36a8c62815
YutoYasunaga <yuto.yasunaga@gmail.com>
2018-09-08 12:10:20 +0900

    Modified category model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 
 describe Category, type: :model do
+  before do
+    @first_category = Category.create(
+      en: 'Animals',
+      vi: 'Động Vật',
+      slug: 'animals'
+    )
+  end
 
   it 'has a valid factory' do
     expect(FactoryBot.build(:category)).to be_valid
   end
 
-  it 'is valid with a English title and Vietnamese title' do
-    category = Category.new(
-      en: 'Vegetables',
-      vi: 'Rau Củ',
-      slug: 'vegetables'
-    )
-    expect(category).to be_valid
+  it 'is valid with a English title, Vietnamese title' do
+    expect(@first_category).to be_valid
   end
 
-  it 'is invalid without English title' do
+  it 'is invalid without a English title' do
     category = FactoryBot.build(:category, en: nil)
     category.valid?
     expect(category.errors[:en]).to include("can't be blank")
   end
 
-  it 'is invalid without Vietnamese title' do
+  it 'is invalid without a Vietnamese title' do
     category = FactoryBot.build(:category, vi: nil)
     category.valid?
     expect(category.errors[:vi]).to include("can't be blank")
   end
 
   it 'is invalid with a duplicate English title' do
-    exist_category = FactoryBot.create(:category, en: 'Vehicle')
-    category = FactoryBot.build(:category, en: 'Vehicle')
+    category = FactoryBot.build(:category, en: 'Animals')
     category.valid?
-    expect(category.errors[:en]).to include("has already been taken")
+    expect(category.errors[:en]).to include('has already been taken')
   end
 
   it 'is invalid with a duplicate Vietnamese title' do
-    exist_category = FactoryBot.create(:category, vi: 'Xe Cộ')
-    category = FactoryBot.build(:category, vi: 'Xe Cộ')
+    category = FactoryBot.build(:category, vi: 'Động Vật')
     category.valid?
-    expect(category.errors[:vi]).to include("has already been taken")
+    expect(category.errors[:vi]).to include('has already been taken')
   end
 
 end
